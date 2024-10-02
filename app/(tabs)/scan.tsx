44715f40ce03cc63e485575bbfc7171822707cff
ismailone23 <ismailhsanprsnl@gmail.com>
2024-10-03 00:19:31 +0600

    scan page expo camera complete

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,63 +1,67 @@
-// import { View, Text, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native'
-// import React, { useState } from 'react'
-// import { CameraType, CameraView, useCameraPermissions } from 'expo-camera'
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-// export default function scan() {
-//     const [permission, requestPermission] = useCameraPermissions()
-//     const [facing, setFacing] = useState<CameraType>('back');
+export default function App() {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
 
-//     if (!permission) return <View />
-//     if (!permission.granted) {
-//         return (
-//             <View className='w-full h-full flex items-center justify-center'>
-//                 <TouchableHighlight className='bg-blue-500' onPress={requestPermission}><Text>Open Camera</Text></TouchableHighlight>
-//             </View>
-//         )
-//     }
-//     function toggleCameraFacing() {
-//         setFacing(current => (current === 'back' ? 'front' : 'back'));
-//     }
-//     return (
-//         <View>
-//             <CameraView facing='back' style={styles.camera}>
-//                 <View style={styles.buttonContainer}>
-//                     <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-//                         <Text style={styles.text}>Flip Camera</Text>
-//                     </TouchableOpacity>
-//                 </View>
-//             </CameraView>
-//         </View>
-//     )
-// }
-// const styles = StyleSheet.create({
-//     camera: {
-//         flex: 1,
-//     },
-//     buttonContainer: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         backgroundColor: 'transparent',
-//         margin: 64,
-//     },
-//     button: {
-//         flex: 1,
-//         alignSelf: 'flex-end',
-//         alignItems: 'center',
-//     },
-//     text: {
-//         fontSize: 24,
-//         fontWeight: 'bold',
-//         color: 'white',
-//     },
-// });
+    if (!permission) {
+        return <View />;
+    }
 
-import { View, Text } from 'react-native'
-import React from 'react'
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
 
-export default function Scan() {
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
     return (
-        <View>
-            <Text>Scan</Text>
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
